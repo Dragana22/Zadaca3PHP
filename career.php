21084a7a15ae6fcfb63a8867268c1eeb29664bc2
Dragana22 <94802673+Dragana22@users.noreply.github.com>
2021-11-25 21:11:29 +0100

    Add files via upload

---
+++ career.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+   include "menu.php";
+?>
+<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.</p>
+</body>
+</html>
---
+++ menu.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+
+<nav>
+        <ul>
+        
+            <li>
+                <a href="http://localhost/draganadavidovic/16/">Home</a>
+            <li>
+                <a href="http://localhost/draganadavidovic/16/career.php">Career</a>
+            </li>
+            <li>
+                <a href="http://localhost/draganadavidovic/16/about.php">About</a>
+            </li>
+        
+        </ul>
+  </nav>
